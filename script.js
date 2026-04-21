4d621304448312bd8bb4234459ae071d0a5fa18c
vanikrishna-dev <vanikrishna02@gmail.com>
2026-04-21 11:49:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 function createTable() {
-    //Write your code here
-  
+    const table = document.getElementById("myTable");
+
+    table.innerHTML = "";
+
+    let rn = prompt("Input number of rows");
+    let cn = prompt("Input number of columns");
+
+    rn = Number(rn);
+    cn = Number(cn);
+
+    if (
+        isNaN(rn) ||
+        isNaN(cn) ||
+        rn <= 0 ||
+        cn <= 0
+    ) {
+        alert("Please enter valid positive numeric values");
+        return;
+    }
+
+    for (let i = 0; i < rn; i++) {
+        let row = table.insertRow(i);
+
+        for (let j = 0; j < cn; j++) {
+            let cell = row.insertCell(j);
+            cell.innerText = `Row-${i} Column-${j}`;
+        }
+    }
 }
